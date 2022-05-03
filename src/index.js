5f159dc5ad8cd469879708044329323eec7ebb87
MarisolHO <101591119+MarisolHO@users.noreply.github.com>
2022-05-03 12:36:38 -0500

    Added a relevant icon from API

--- src/index.js
+++ src/index.js
@@ -47,6 +47,11 @@ function showTemperature(response) {
 
   let h1 = document.querySelector("#city");
   h1.innerHTML = response.data.name;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(event) {
